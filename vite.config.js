8c48de157fe10c6185e06d54a370156e3e4e06d0
KosuriPavan <106147230+KosuriPavan@users.noreply.github.com>
2025-04-04 20:53:56 +0530

    tryout

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  // base: "/new-portfolio/",
+export default defineConfig(({ command }) => ({
+  base: command === "build" ? "/new-portfolio/" : "/",
   plugins: [react()],
   server: {
     open: true,
   },
-});
+}));
