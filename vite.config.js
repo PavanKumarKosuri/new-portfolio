d9b549d5c626d752f5fc4b486f76d68ec8e4af31
KosuriPavan <106147230+KosuriPavan@users.noreply.github.com>
2025-04-17 00:06:21 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ command }) => ({
   base: command === "build" ? "/new-portfolio/" : "/",
+  build: {
+    outDir: 'docs',
+  },
   plugins: [react()],
   server: {
     open: true,
